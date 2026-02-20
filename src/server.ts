9ea6df273276cfa245d2ea3f4cd4fcfca02c645d
Anderson Miyashita <andsht@gmail.com>
2026-02-20 18:50:05 -0300

    Server running and AdminJS set up

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { sequelize } from "./database";
+import { adminJs, adminJSRouter } from "./adminjs";
 
 const app = express();
+
+app.use(adminJs.options.rootPath, adminJSRouter);
+
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000;
 
