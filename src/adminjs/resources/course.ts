6a4f0f32d0c0ee8e6b7e0159c6eb2dbb5af02e74
Anderson Miyashita <andsht@gmail.com>
2026-02-21 23:26:31 -0300

    Tabela users - super user created

---
+++ src/adminjs/resources/course.ts
@@ -0,0 +1,32 @@
+// src/adminjs/resources/course.ts
+
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from 'path';
+
+export const courseResourceOptions: ResourceOptions = {
+  parent: {name: 'Catálogo'},
+
+  editProperties: ['name', 'synopsis', 'uploadThumbnail', 'featured', 'categoryId'],
+  filterProperties: ['name', 'synopsis', 'featured', 'categoryId', 'createdAt', 'updatedAt'],
+  listProperties: ['id', 'name', 'featured', 'categoryId'],
+  showProperties: ['id', 'name', 'synopsis', 'featured', 'thumbnailUrl', 'categoryId', 'createdAt', 'updatedAt']
+}
+
+// que entre as featuretypes já existem
+// objetos para aws ou google cloud service
+export const courseResourceFeatures: FeatureType [] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, '..', '..', '..', 'public')
+      }
+    },
+    properties: {
+      key: 'thumbnailUrl',
+      file: 'uploadThumbnail'
+    },
+    uploadPath: (record, filename) => `thumbnails/course-${record.get('id')}/${filename}`
+  })
+]
+
