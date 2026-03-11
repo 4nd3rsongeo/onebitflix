6d182fc26acb93223a1d9f6cf7d51efb63829f43
Anderson Miyashita <andsht@gmail.com>
2026-03-11 06:42:04 -0300

    mantido dot admin

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,17 @@ dotenv.config()
 import express from "express";
 import cors from "cors";
 import { sequelize } from "./database";
+import AdminJS from 'adminjs'
 import { adminJs, adminJSRouter } from "./adminjs";
+
 import { router } from "./routes";
 
 //preparado para deploy
 const app = express();
 
 app.use(cors())
+
+
 
 app.use(adminJs.options.rootPath, adminJSRouter);
 
